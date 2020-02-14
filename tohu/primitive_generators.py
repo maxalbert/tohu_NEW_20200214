3c82311ff564bcd5be7d2ef4be46e239c5dbd6b1
Maximilian Albert <maximilian.albert@gmail.com>
2020-02-14 22:55:52 +0000

    Add __repr__ methods

--- tohu/primitive_generators.py
+++ tohu/primitive_generators.py
@@ -15,6 +15,9 @@ class Constant:
     def __init__(self, value):
         self.value = value
 
+    def __repr__(self):
+        return f"<Constant: {self.value!r}>"
+
     def reset(self, seed):
         """
         Note that the value of the `seed` argument is ignored because
@@ -47,6 +50,9 @@ class Boolean:
         self.randgen = Random()
         self.dtype = bool
 
+    def __repr__(self):
+        return f"<Boolean: p={self.p!r}>"
+
     def reset(self, seed):
         # super().reset(seed)
         self.randgen.seed(seed)
@@ -73,6 +79,9 @@ class Integer:
         self.low = low
         self.high = high
         self.randgen = Random()
+
+    def __repr__(self):
+        return f"<Integer: low={self.low!r}, high={self.high!r}>"
 
     def reset(self, seed):
         self.randgen.seed(seed)
@@ -123,6 +132,9 @@ class HashDigest:
         self._maybe_convert_to_uppercase = identity if (self.as_bytes or lowercase) else str.upper
         self.dtype = str
 
+    def __repr__(self):
+        return f"<HashDigest: length={self.length!r}, as_bytes={self.as_bytes!r}, lowercase={self.lowercase!r}>"
+
     def reset(self, seed):
         self.randgen.seed(seed)
         return self
@@ -162,6 +174,9 @@ class FakerGenerator:
         self.fake = Faker(locale=locale)
         self.randgen = getattr(self.fake, method)
         self.fake.seed_instance(None)  # seed instance to ensure we are decoupled from the global random state
+
+    def __repr__(self):
+        return f"<FakerGenerator: method={self.method!r}, locale={self.locale!r}, faker_args={self.faker_args!r}>"
 
     def reset(self, seed):
         # super().reset(seed)
