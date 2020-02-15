88040fec8391e0f978cddfd234522dff9ef2cc1c
Maximilian Albert <maximilian.albert@gmail.com>
2020-02-15 23:12:26 +0000

    numpydocify

--- tohu/utils.py
+++ tohu/utils.py
@@ -5,7 +5,8 @@ def print_generated_sequence(gen, num, *, seed, sep=", ", fmt=""):
     """
     Helper function which prints a sequence of `num` item produced by the random generator `gen`.
 
-    Example:
+    Examples
+    --------
     >>> g = Integer(1, 20)
     >>> print_generated_sequence(g, num=10, seed=99999, fmt='02d', sep=", ")
     Generated sequence: 06, 07, 18, 12, 19, 05, 17, 15, 09, 18
