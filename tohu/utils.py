88f8f2ee2def339ebf2c5894bf01fe16f93a57a6
Maximilian Albert <maximilian.albert@gmail.com>
2020-02-15 20:05:45 +0000

    Make seed argument mandatory

--- tohu/utils.py
+++ tohu/utils.py
@@ -1,7 +1,7 @@
 __all__ = ["print_generated_sequence", "identity"]
 
 
-def print_generated_sequence(gen, num, *, sep=", ", fmt="", seed=None):
+def print_generated_sequence(gen, num, *, seed, sep=", ", fmt=""):
     """
     Helper function which prints a sequence of `num` item produced by the random generator `gen`.
 
@@ -10,8 +10,7 @@ def print_generated_sequence(gen, num, *, sep=", ", fmt="", seed=None):
     >>> print_generated_sequence(g, num=10, seed=99999, fmt='02d', sep=", ")
     Generated sequence: 06, 07, 18, 12, 19, 05, 17, 15, 09, 18
     """
-    if seed:
-        gen.reset(seed)
+    gen.reset(seed)
 
     elems = [format(next(gen), fmt) for _ in range(num)]
     sep_initial = "\n\n" if "\n" in sep else " "
