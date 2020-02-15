8c06095f711297004b165accc68acf31edd83a28
Maximilian Albert <maximilian.albert@gmail.com>
2020-02-15 21:16:56 +0000

    Move TohuBaseGenerator into separate file

--- tohu/primitive_generators.py
+++ tohu/primitive_generators.py
@@ -1,76 +1,12 @@
-import hashlib
 import numpy as np
 import warnings
 from faker import Faker
-from itertools import islice
 from random import Random
-from tqdm import tqdm
+
+from .base import TohuBaseGenerator
 from .utils import identity
 
 __all__ = ["Constant", "Boolean", "Integer", "HashDigest", "FakerGenerator"]
-
-
-class TohuBaseGenerator:
-    """
-    Base class for all of tohu's generators.
-    """
-
-    def __init__(self):
-        self.tohu_name = None
-
-    def __repr__(self):
-        clsname = self.__class__.__name__
-        name = "" if self.tohu_name is None else f"{self.tohu_name}: "
-        return f"<{name}{clsname} (id={self.tohu_id})>"
-
-    def __iter__(self):
-        return self
-
-    def set_tohu_name(self, tohu_name):
-        """
-        Set this generator's `tohu_name` attribute.
-
-        This is mainly useful for debugging where one can temporarily
-        use this at the end of generator definitions to set a name
-        that will be displayed in debugging messages. For example:
-
-            g1 = SomeGeneratorClass().set_tohu_name('g1')
-            g2 = SomeGeneratorClass().set_tohu_name('g2')
-        """
-        self.tohu_name = tohu_name
-        return self
-
-    @property
-    def tohu_id(self):
-        """
-        Return (truncated) md5 hash representing this generator.
-        We truncate the hash simply for readability, as this is
-        purely intended for debugging purposes and the risk of
-        any collisions will be negligible.
-        """
-        myhash = hashlib.md5(str(id(self)).encode()).hexdigest()
-        return myhash[:6]
-
-    def generate_as_stream(self, num, *, seed=None, progressbar=False):
-        """
-        Return sequence of `num` elements.
-
-        If `seed` is not None, the generator is reset
-        using this seed before generating the elements.
-        """
-        if seed is not None:
-            self.reset(seed)
-
-        items = islice(self, num)
-        if progressbar:  # pragma: no cover
-            items = tqdm(items, total=num)
-
-        yield from items
-
-    def generate_as_list(self, num, *, seed=None, progressbar=False):
-        return list(self.generate(num, seed=seed, progressbar=progressbar))
-
-    generate = generate_as_stream  # convenience alias
 
 
 class Constant(TohuBaseGenerator):
