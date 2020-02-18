1d9e40240ef04e9c77525698aef2287815343c77
Maximilian Albert <maximilian.albert@gmail.com>
2020-02-18 23:21:30 +0000

    Tweak helper function to allow new-style formatting and avoiding to reset if desired

--- tohu/utils.py
+++ tohu/utils.py
@@ -1,19 +1,20 @@
 __all__ = ["print_generated_sequence", "identity"]
 
 
-def print_generated_sequence(gen, num, *, seed, sep=", ", fmt=""):
+def print_generated_sequence(gen, num, *, seed=None, sep=", ", fmt="{}"):
     """
     Helper function which prints a sequence of `num` item produced by the random generator `gen`.
 
     Examples
     --------
     >>> g = Integer(1, 20)
-    >>> print_generated_sequence(g, num=10, seed=99999, fmt='02d', sep=", ")
+    >>> print_generated_sequence(g, num=10, seed=99999, fmt='{:02d}', sep=", ")
     Generated sequence: 06, 07, 18, 12, 19, 05, 17, 15, 09, 18
     """
-    gen.reset(seed)
+    if seed is not None:
+        gen.reset(seed)
 
-    elems = [format(next(gen), fmt) for _ in range(num)]
+    elems = [fmt.format(next(gen)) for _ in range(num)]
     sep_initial = "\n\n" if "\n" in sep else " "
     print("Generated sequence:{}{}".format(sep_initial, sep.join(elems)))
 
