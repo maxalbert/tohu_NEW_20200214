42c3e16f5ade8535414f4c89283741b92517173d
Maximilian Albert <maximilian.albert@gmail.com>
2020-03-01 19:57:41 +0000

    Allow loop variables to have values assigned later rather than during initialisation

--- tohu/looping.py
+++ tohu/looping.py
@@ -2,21 +2,40 @@ from typing import Sequence
 from .base import TohuBaseGenerator
 
 
+class UnassignedValuesError(Exception):
+    pass
+
+
 class LoopVariable(TohuBaseGenerator):
-    def __init__(self, name, values):
-        if not isinstance(values, Sequence) or isinstance(values, str):
-            raise TypeError(f"Argument `values` must be a list, tuple, or similar sequence type. Got: {type(values)}")
+    def __init__(self, name, values=None):
         super().__init__()
         self.name = name
-        self.values = values
-        self.idx = 0
-        self.cur_value = self.values[0]
-        self.is_hidden = True
+        self.assign_values(values)
+
+    def assign_values(self, values):
+        if values is not None and (not isinstance(values, Sequence) or isinstance(values, str)):
+            raise TypeError(f"Argument `values` must be a list, tuple, or similar sequence type. Got: {type(values)}")
+
+        if values:
+            self.values = values
+            self.idx = 0
+            self.cur_value = self.values[0]
+            self.has_values_assigned = True
+            self.is_hidden = True
+        else:
+            self.values = None
+            self.idx = None
+            self.cur_value = None
+            self.has_values_assigned = False
+            self.is_hidden = True
 
     def __next__(self):
         return self.cur_value
 
     def reset(self, seed=None):
+        if not self.has_values_assigned:
+            raise UnassignedValuesError(f"Loop variable {self.name!r} has not been assigned any values.")
+
         # Note: There are two options for what reset() can do in a LoopVariable.
         # Currently we reset it to its first value (which is consistent with the
         # reset behaviour of other tohu generators). As a consequence, we can only
@@ -34,6 +53,9 @@ class LoopVariable(TohuBaseGenerator):
         self.cur_value = self.values[0]
 
     def advance(self):
+        if not self.has_values_assigned:
+            raise UnassignedValuesError(f"Loop variable {self.name!r} has not been assigned any values.")
+
         self.idx += 1
         try:
             self.cur_value = self.values[self.idx]
