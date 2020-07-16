a2398257267ff5dcd497e8cf398765023c1d2192
Maximilian Albert <maximilian.albert@gmail.com>
2020-07-16 21:13:54 +0100

    Tweak method/attribute names in TohuNamespaceNEW3; add convenience method

--- tohu/tohu_namespace_NEW_3.py
+++ tohu/tohu_namespace_NEW_3.py
@@ -1,4 +1,4 @@
-from .base import SeedGenerator
+from .base import SeedGenerator, is_tohu_generator
 
 
 __all__ = ["TohuNamespaceNEW3"]
@@ -7,26 +7,31 @@ __all__ = ["TohuNamespaceNEW3"]
 class TohuNamespaceNEW3:
     def __init__(self, dependency_mapping=None):
         self.dependency_mapping = dependency_mapping or {}
-        self.generators = {}
+        self.field_generators = {}
         self.seed_generator = SeedGenerator()
 
     def __format__(self, format_specifier):
         s = f"<{self.__class__.__name__}\n"
-        for name, g in self.generators.items():
+        for name, g in self.field_generators.items():
             s += f"   {name!r}: {g:debug}\n"
         s += f">"
         return s
 
-    def add_spawn_of(self, g, *, name):
+    def add_field_generators_from_dict(self, dct):
+        for name, g in dct.items():
+            if is_tohu_generator(g):
+                self.add_field_generator(g, name=name)
+
+    def add_field_generator(self, g, *, name):
         g_spawned = g.spawn(self.dependency_mapping)
         self.dependency_mapping[g] = g_spawned
-        self.generators[name] = g_spawned
+        self.field_generators[name] = g_spawned
 
     def reset(self, seed):
         self.seed_generator.reset(seed)
 
-        for g in self.generators.values():
+        for g in self.field_generators.values():
             g.reset(next(self.seed_generator))
 
     def __next__(self):
-        return tuple(next(g) for g in self.generators.values())
+        return tuple(next(g) for g in self.field_generators.values())
