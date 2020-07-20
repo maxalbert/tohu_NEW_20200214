0c883d584f2a119475184a920288e33a2d942daf
Maximilian Albert <maximilian.albert@gmail.com>
2020-07-20 21:32:55 +0100

    Add basic versions of LoopedCustomGeneratorClass, LoopedCustomGeneratorInstance (missing loop var values are not yet supported, and some sanity checks are missing)

---
+++ tohu/looped_custom_generator.py
@@ -0,0 +1,58 @@
+import inspect
+
+from .custom_generator_NEW_3 import CustomGeneratorNEW3
+from .logging import logger
+from .looping_NEW_3 import LoopRunnerNEW3
+
+
+class LoopedCustomGeneratorInstance:
+    def __init__(self, loop_runner, custom_gen_instance):
+        self.loop_runner = loop_runner
+        self.custom_gen_instance = custom_gen_instance
+
+    def __repr__(self):
+        return f"<{self.__class__.__name__} wrapping {self.custom_gen_instance}>"
+
+    def generate_as_stream(self, *, num_items_per_loop_iteration, seed=None):
+        logger.warning(
+            "TODO: ensure that all loop variables have been assigned values (and perform any other sanity checks)!"
+        )
+
+        yield from self.loop_runner.produce_items_from_tohu_generator(
+            self.custom_gen_instance, num_items_per_loop_iteration, seed
+        )
+
+        # raise NotImplementedError("TODO: implement me!")
+
+
+class LoopedCustomGeneratorClass:
+    def __init__(self, custom_gen_cls_or_looped_custom_gen_cls):
+        if inspect.isclass(custom_gen_cls_or_looped_custom_gen_cls) and issubclass(
+            custom_gen_cls_or_looped_custom_gen_cls, CustomGeneratorNEW3
+        ):
+            self.custom_gen_cls = custom_gen_cls_or_looped_custom_gen_cls
+            self.loop_runner = LoopRunnerNEW3()
+            self.cur_level = 0
+        elif isinstance(custom_gen_cls_or_looped_custom_gen_cls, LoopedCustomGeneratorClass):
+            self.custom_gen_cls = custom_gen_cls_or_looped_custom_gen_cls.custom_gen_cls
+            self.loop_runner = custom_gen_cls_or_looped_custom_gen_cls.loop_runner
+            self.cur_level = custom_gen_cls_or_looped_custom_gen_cls.cur_level
+        else:
+            raise TypeError(
+                f"Cannot instantiate {self.__class__.__name__} with object of type {type(custom_gen_cls_or_looped_custom_gen_cls)}"
+            )
+
+    def __repr__(self):
+        return f"<{self.__class__.__name__}: loop_vars={self.loop_runner.loop_variables}, cur_level={self.cur_level}>"
+
+    def add_loop_variables_at_next_level_up(self, loop_vars):
+        next_level_up = self.cur_level + 1
+        self.loop_runner.add_loop_variables_at_level(loop_vars, level=next_level_up)
+        self.cur_level = next_level_up
+
+    def __call__(self, *args, **kwargs):
+        loop_runner_spawned, loop_var_dep_mapping = self.loop_runner.spawn()
+        logger.debug(f"loop_var_dep_mapping keys: {list(loop_var_dep_mapping.keys())}")
+        self.custom_gen_cls.set_dependency_mapping_for_next_instance_creation(loop_var_dep_mapping)
+        custom_gen_instance = self.custom_gen_cls(*args, **kwargs)
+        return LoopedCustomGeneratorInstance(loop_runner_spawned, custom_gen_instance)
