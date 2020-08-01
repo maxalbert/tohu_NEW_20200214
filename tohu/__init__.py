b2636e6f08483dcaeeef6eceda49da36ae1975eb
Maximilian Albert <maximilian.albert@gmail.com>
2020-08-01 22:04:50 +0100

    Remove obsolete toplevel imports

--- tohu/__init__.py
+++ tohu/__init__.py
@@ -4,10 +4,7 @@ Your friendly synthetic data generator
 
 from .primitive_generators import *
 from .derived_generators import Apply
-from .custom_generator import CustomGenerator
-from .custom_generator_NEW import CustomGeneratorNEW
 from .custom_generator_NEW_3 import CustomGeneratorNEW3
-from .foreach import foreach
 from .foreach_NEW_3 import foreach_NEW3
 from .logging import logger as tohu_logger
 
